eca57a055bbbd86254463e339e0fffaf0b02dc75
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-24 22:11:19 +0100

    get user api

--- with cdn/app.js
+++ with cdn/app.js
@@ -1,11 +1,31 @@
-const app = Vue.createApp({
-    template: `<h1>Hello {{framework}}</h1>`,
-    data() {
-        return {
-            framework: 'Vue!'
-        }
+const data = () => {
+    return {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'jonh@gmail.com',
+        gender: 'male',
+        picture: `https://randomuser.me/api/portraits/men/10.jpg`
     }
+}
 
+async function getUser (){
+    const res = await fetch('https://randomuser.me/api')
+    const { results } = await res.json()
+    this.firstname = results[0].name.first
+    this.lastname = results[0].name.last
+    this.email = results[0].email
+    this.picture = results[0].picture.large;
+    this.gender = results[0].gender
+    console.log(this)
+}
+
+const methods = {
+    getUser,
+}
+
+const app = Vue.createApp({
+    data,
+    methods
 })
 
 app.mount('#app')
