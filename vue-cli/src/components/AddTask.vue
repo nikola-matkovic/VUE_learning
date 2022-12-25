eb4f5b74448c51f3db26ec2554566fa211075f31
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-25 13:34:26 +0100

    add dasks form

---
+++ vue-cli/src/components/AddTask.vue
@@ -0,0 +1,23 @@
+<template>
+    <form class="add-fomr">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" placeholder="Add Task" />
+        </div>
+        <div class="form-control">
+            <label>Day & Time</label>
+            <input type="text" placeholder="Add Day & Time" />
+        </div>
+        <div class="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" />
+        </div>
+        <input type="submit" value="Save Task" class="btn btn-block" />
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'AddTask'
+}
+</script>
