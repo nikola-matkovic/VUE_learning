14371237d3378d698445fcecbc4c9fdffaeb826c
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:41:47 +0100

    Watch

---
+++ tutorial/watch1.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            todoId: 1,
+            todoData: null,
+        };
+    },
+    methods: {
+        async fetchData() {
+            this.todoData = null;
+            const res = await fetch(
+                `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
+            );
+            this.todoData = await res.json();
+        },
+    },
+    mounted() {
+        this.fetchData();
+    },
+    watch: {
+        todoId() {
+            this.fetchData();
+        },
+    },
+};
+</script>
+
+<template>
+    <p>Todo id: {{ todoId }}</p>
+    <button @click="todoId++">Fetch next todo</button>
+    <p v-if="!todoData">Loading...</p>
+    <pre v-else>{{ todoData }}</pre>
+</template>
