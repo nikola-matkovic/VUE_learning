70d30710ca388c20048add3da62524f9376bb5e1
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:40:11 +0100

    conditional rendering

---
+++ tutorial/Conditional Rendering1.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            awesome: true,
+        };
+    },
+    methods: {
+        toggle() {
+            this.awesome = !this.awesome;
+        },
+    },
+};
+</script>
+
+<template>
+    <button @click="toggle">toggle</button>
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+</template>
