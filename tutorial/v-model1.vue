d7f7edb3e97bea6d5cbb348954776c2a20cb7fdd
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:40:32 +0100

    v model

---
+++ tutorial/v-model1.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const text = ref("");
+
+function onInput(e) {
+    text.value = e.target.value;
+}
+</script>
+
+<template>
+    <input v-model="text" placeholder="Type here" />
+    <p>{{ text }}</p>
+</template>
---
+++ tutorial/v-model2.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            text: "",
+        };
+    },
+};
+</script>
+
+<template>
+    <input v-model="text" placeholder="Type here" />
+    <p>{{ text }}</p>
+</template>
