c0c332d9bc86470ea98cbc95174cadc6e7aa5562
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:52:02 +0100

    init project

---
+++ Roman number converter/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template></template>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    min-height: 100vh;
+}
+</style>
