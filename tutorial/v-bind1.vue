2f30eb759ac30f3b8ef21692aa0a578421916937
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 14:00:54 +0100

    vbin learning

---
+++ tutorial/v-bind1.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            titleClass: "title",
+        };
+    },
+};
+</script>
+
+<template>
+    <h1 :class="titleClass">Make me red</h1>
+    <!-- add dynamic class binding here -->
+</template>
+
+<style>
+.title {
+    color: red;
+}
+</style>
