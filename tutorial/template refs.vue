4585925bb38d81cc2231fb16fc4cc3b1df584ce7
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:40:23 +0100

    template refs

---
+++ tutorial/template refs.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    mounted() {
+        this.$refs.p.textContent = "How are you?";
+    },
+};
+</script>
+
+<template>
+    <p ref="p">hello</p>
+</template>
---
+++ tutorial/templete refs2.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const p = ref(null);
+
+onMounted(() => (p.value.textContent = "fine"));
+</script>
+
+<template>
+    <p ref="p">hello</p>
+</template>
