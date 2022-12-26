5951ccbf35fda012bc318a28bd33ffe3ed5b99f3
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 14:01:49 +0100

    vbind 2

---
+++ tutorial/v-bind2.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const titleClass = ref("title");
+</script>
+
+<template>
+    <h1 :class="titleClass">Make me red</h1>
+    <!-- add dynamic class binding here -->
+</template>
+
+<style>
+.title {
+    color: red;
+}
+</style>
