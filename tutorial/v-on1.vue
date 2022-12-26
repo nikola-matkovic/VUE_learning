115ec55f8baf36d2c1b8fafcc9fe26511cd851ad
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 16:40:53 +0100

    events

---
+++ tutorial/v-on1.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+    },
+};
+</script>
+
+<template>
+    <!-- make this button work -->
+    <button @click="increment">count is: {{ count }}</button>
+</template>
---
+++ tutorial/v-on2.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+function increment() {
+    count.value++;
+}
+</script>
+
+<template>
+    <!-- make this button work -->
+    <button @click="increment">count is: {{ count }}</button>
+</template>
