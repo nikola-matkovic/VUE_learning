9a1c7d8bf35fee7a66d98b55ed71980f6719cc00
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-26 21:18:04 +0100

    bindings

--- learning-from-docs/src/views/vBindView.vue
+++ learning-from-docs/src/views/vBindView.vue
@@ -1,6 +1,51 @@
 <template>
-    <main>
-        <h1>Home page</h1>
-        <p>Home page body content</p>
+    <main v-bind="objectOfBindings">
+        <h1>Learning bindings</h1>
+        <p>I'm binding multiple attributes</p>
+        <div :="objectOfBindings2">
+            <p>Binding a class</p>
+            <p>Binding a style</p>
+        </div>
     </main>
 </template>
+
+<script setup>
+const objectOfBindings = {
+    id: "bindingPage",
+    class: "h1",
+    style: {
+        border: "1px solid black",
+    },
+};
+
+const objectOfBindings2 = {
+    id: "div",
+    class: "div",
+};
+</script>
+
+<style scoped>
+#bindingPage {
+    background: rgb(224, 95, 95);
+    color: white;
+}
+.h1 {
+    font-size: 40px;
+}
+
+.div {
+    background: blue;
+    width: 300px;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+}
+
+#div {
+    border-radius: 50%;
+    margin: 40px;
+}
+</style>
