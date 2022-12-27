6018b986fd93515caa3324b4e1067aa54b2bbb9b
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-27 18:32:01 +0100

    Computed with setter

--- learning-from-docs/src/views/ComputedView.vue
+++ learning-from-docs/src/views/ComputedView.vue
@@ -1,5 +1,7 @@
 <template>
     <div>Persos is {{ isAdult ? "" : "Not" }} Adult</div>
+    <button @click="changeName">Change name</button>
+    <p>{{ fullName }}</p>
 </template>
 
 <script setup>
@@ -11,9 +13,25 @@ const person = ref({
     age: 20,
 });
 
+const firstName = ref("Jovana");
+const lastName = ref("Jovanović");
+
+const fullName = computed({
+    get() {
+        return firstName.value + " " + lastName.value;
+    },
+    set(newValue) {
+        [firstName.value, lastName.value] = newValue.split(" ");
+    },
+});
+
 const isAdult = computed(() => {
     return person.value.age >= 18;
 });
+
+function changeName() {
+    fullName.value = "Jovana Matković";
+}
 </script>
 
 <style scoped></style>
