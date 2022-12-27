e5f7fc0790e47691912764d700e5a8be26ec4f28
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-27 18:25:23 +0100

    computd props

--- learning-from-docs/src/views/ComputedView.vue
+++ learning-from-docs/src/views/ComputedView.vue
@@ -1,40 +1,19 @@
 <template>
-    <form @submit.prevent="hello">
-        <input v-model="text" type="text" placeholder="your name" />
-        <button>Send</button>
-    </form>
+    <div>Persos is {{ isAdult ? "" : "Not" }} Adult</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
-const text = ref("");
+const person = ref({
+    name: "Nikola",
+    surname: "Matković",
+    age: 20,
+});
 
-const hello = () => {
-    alert("Hello " + text.value);
-};
+const isAdult = computed(() => {
+    return person.value.age >= 18;
+});
 </script>
 
-<style scoped>
-form {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin: 40px;
-}
-input {
-    width: 300px;
-    height: 40px;
-    border-radius: 5px;
-    border: 1px solid black;
-    margin: 10px;
-}
-button {
-    width: 300px;
-    height: 40px;
-    border-radius: 5px;
-    border: 1px solid black;
-    margin: 10px;
-}
-</style>
+<style scoped></style>
