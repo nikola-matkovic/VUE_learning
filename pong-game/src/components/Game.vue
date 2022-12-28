db82765cebafcd83d9b894453e01856a5a3d4884
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-28 14:21:34 +0100

    add style for left player

--- pong-game/src/components/Game.vue
+++ pong-game/src/components/Game.vue
@@ -1,5 +1,32 @@
-<template>test</template>
+<template>
+    <div class="game-container">
+        <div id="left"></div>
+        <div id="right"></div>
+    </div>
+</template>
 
 <script setup></script>
 
-<style></style>
+<style>
+body,
+#app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.game-container {
+    width: 95vmin;
+    height: 95vmin;
+    background-color: #100101;
+    position: relative;
+}
+
+#left {
+    background: red;
+    width: 20px;
+    height: 100px;
+    position: absolute;
+    left: 10px;
+    top: 0;
+}
+</style>
