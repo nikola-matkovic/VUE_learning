990f055d268dcc34eb443e5d947f519dc358e94e
anonymousvegan <majjunjulavgust@gmail.com>
2022-12-29 13:26:14 +0100

    add player

--- dino-meme-game/src/components/Game.vue
+++ dino-meme-game/src/components/Game.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="game">
         <div id="land"></div>
+        <div id="player" ref="player"></div>
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
+const player = ref(null);
 </script>
 
 <style>
